48b000a68dae7051818fa8817126dd02a10ccc0d
Jeffrey Magouirk <jmagouirk@earthlink.net>
2020-11-23 14:59:34 -0600

    Add files via upload

--- Compare_text.py
+++ Compare_text.py
@@ -1,15 +1,33 @@
 ###File to compare to text files.
+
+
 import difflib
-
-with open('c:\\Users\\Jeffrey.Magouirk\\Downloads\\202007_Job 2 - NUMERATOR_AFCHIPS4_sharepoint_version.txt', 'r') as file1:
-    f1_text = file1.read()
+import os
 
 
-with open('c:\\Users\\Jeffrey.Magouirk\\Downloads\\Job 2 - NUMERATOR_AFCHIPS4_J5_transition.txt', 'r') as file2:
-    f2_text = file2.read()    
+sharepoint = 'c:\\Users\\Jeffrey.Magouirk\\Downloads\\HEDIS FUH 2020_J5_transition\\HEDIS FUH 2020\\FUH\\202007'
+QA = 'c:\\Users\\Jeffrey.Magouirk\\Downloads\\HEDIS FUH 2020_J5_transition\\HEDIS FUH 2020\\FUH\\202007\\2020QA'
+list_files = os.listdir(sharepoint)
+list_filesQA = os.listdir(QA)
+print('Transition files = ',list_files)
+print('QA files =', list_filesQA)
+n1 = len(list_filesQA)
+print(n1)
+n = 1
+for i in range(n,n1):
+    c1 = sharepoint + '\\' + list_files[i + 1]
+    print(c1)
 
 
-for line in difflib.unified_diff(f1_text, f2_text, fromfile='file1',
-                                 tofile='file2',lineterm=''):
-    print(line)
+    c2 = QA + '\\' + list_filesQA[i]
+    print(c2)
 
+    with open(c1,'r') as file0:
+        f0_text = file0.read()
+
+    with open(c2, 'r') as file0a:
+        f0a_text = file0a.read()
+
+    d = difflib.Differ()
+    diff = d.compare(f0_text,f0a_text)
+    print(list_files[i+1],'=',list_filesQA[i],'\n'.join(diff))
